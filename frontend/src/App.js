0fabd5ccb610a02cf341863960c09da8433eda59
Juliana Cristina <julianacristina780@gmail.com>
2020-06-29 19:18:16 -0300

    Adicionando o cors no backend e Listando os projetos da API no front.

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import React , {useState, useEffect} from 'react';
+import Header from './components/Header'
+import api from './services/api'
+import './App.css'
+
+function App(){
+
+    const [projects, setProjects] = useState([])
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data)
+        })
+    }, [])
+
+
+    function handleAddProject(){
+        setProjects([...projects, `Novo Projeto ${Date.now()}`])
+    }    
+    
+    return( 
+    <>
+        <Header title="Projects"/>
+            <ul>
+                {projects.map(project => <li key={project.id}>{project.title}</li>)}
+            </ul>
+        <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
+    </>
+    )
+}
+
+export default App
