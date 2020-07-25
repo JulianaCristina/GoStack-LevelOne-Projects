341aff600b99bcadc3b09ab8304beeb9bf53eaba
Juliana Gonçalves <julianacristina780@gmail.com>
2020-07-25 18:10:34 -0300

    Aplicando SOLID

--- Level 2/first-project-nodejs/src/repositories/AppointmentsRepository.ts
+++ Level 2/first-project-nodejs/src/repositories/AppointmentsRepository.ts
@@ -1,6 +1,11 @@
 import Appointment from "../models/Appointment";
 import {isEqual} from "date-fns";
 
+interface CreateAppointmentDTO{
+    provider: string;
+    date: Date;
+}
+
 class AppointmentsRepository {
    private appointments: Appointment[];
 
@@ -8,15 +13,18 @@ class AppointmentsRepository {
        this.appointments = [];
    }
 
+   public all(): Appointment[]{
+       return this.appointments;
+   }
    public findByDate(date: Date): Appointment | null{
        const findAppointment = this.appointments.find(appointment => isEqual(date, appointment.date))
 
        return findAppointment|| null;
    }
 
-   public create(provider: string, date: Date): Appointment{
+   public create({provider, date}: CreateAppointmentDTO): Appointment{
 
-       const appointment = new Appointment(provider, date)
+       const appointment = new Appointment({provider, date})
 
        this.appointments.push(appointment)
 
